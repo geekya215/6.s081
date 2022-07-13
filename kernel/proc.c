d600900e3acd5312d1aea9e6c81d66f119098fa5
Lin Yang <geekya215@gmail.com>
2022-07-13 10:31:25 +0800

    pass trace

--- kernel/proc.c
+++ kernel/proc.c
@@ -164,6 +164,7 @@ freeproc(struct proc *p)
   p->killed = 0;
   p->xstate = 0;
   p->state = UNUSED;
+  p->trace_mask = 0;
 }
 
 // Create a user page table for a given process,
@@ -302,6 +303,8 @@ fork(void)
   np->cwd = idup(p->cwd);
 
   safestrcpy(np->name, p->name, sizeof(p->name));
+
+  np->trace_mask = p->trace_mask;
 
   pid = np->pid;
 
