ac389ceebbdca5e5a2dcbd8643c91bd03a51358d
Lin Yang <geekya215@gmail.com>
2022-08-14 23:19:32 +0800

    pass backtrace

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -60,6 +60,7 @@ sys_sleep(void)
 
   if(argint(0, &n) < 0)
     return -1;
+
   acquire(&tickslock);
   ticks0 = ticks;
   while(ticks - ticks0 < n){
@@ -70,6 +71,8 @@ sys_sleep(void)
     sleep(&ticks, &tickslock);
   }
   release(&tickslock);
+
+  backtrace();
   return 0;
 }
 
